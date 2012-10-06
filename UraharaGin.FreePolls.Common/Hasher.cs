8c16748b494f3eb71271a7ab7935cb44ad3ea6c4
UraharaGin <exaurov.nm@gmail.com>
2012-10-06 19:48:19 +0300

    Added hasher and HashProvider

---
+++ UraharaGin.FreePolls.Common/Hasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace UraharaGin.FreePolls.Common
+{
+    public class Hasher
+    {
+        public string ComputeHash(string str)
+        {
+            var md5 = MD5.Create();
+            var encoding = new System.Text.UTF8Encoding();
+            var encodedString = encoding.GetBytes(str);
+            var hashBytes = md5.ComputeHash(encodedString);
+            var deEncodedHash = encoding.GetChars(hashBytes);
+            return new string(deEncodedHash);
+        }
+    }
+}
